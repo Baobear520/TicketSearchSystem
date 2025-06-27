1d8a6cb77357b71430ac516c05d06e1e5dcae79f
Dima <aldmikon@gmail.com>
2025-06-27 15:08:28 +0300

    Adding typing

--- src/infrastructure/repositories/base.py
+++ src/infrastructure/repositories/base.py
@@ -1,7 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+from src.domain.models.entities import SearchRequest
 
 
 class BaseRepository(ABC):
     @abstractmethod
-    def find_by_request(self, request: SearchRequest):
+    def find_by_request(self, request: SearchRequest)-> List:
         pass
