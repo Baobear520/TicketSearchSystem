3d55f537a64490e1ce082c5e9cc7aa658166c208
Dima <aldmikon@gmail.com>
2025-06-27 15:06:27 +0300

    Refactoring models

--- src/domain/models/entities.py
+++ src/domain/models/entities.py
@@ -1,10 +1,10 @@
 from uuid import UUID
 from dataclasses import dataclass
 from datetime import datetime
-from typing import List, Set, Optional
+from typing import List, Set
 
-from main import UniqueValidator
-from src.domain.models.enums import TicketType, TicketCategory
+
+from src.domain.enums import TicketType, TicketCategory
 
 
 @dataclass
@@ -14,40 +14,23 @@ class Ticket:
     price: float
     category: TicketCategory
 
+
+@dataclass
+class Event:
+    id: str
+    name: str
+    date: datetime
+    ticket_categories: Set[TicketCategory]
+    tickets: List[Ticket] = None
+
 @dataclass
 class Venue:
-    id: UUID
+    id: str
     name: str
     location: str
     existing_ticket_types: Set[TicketType]
     max_capacity: int
-
-
-class Event:
-    def __init__(
-            self,
-            id: str,
-            name: str,
-            venue: Venue,
-            date: datetime,
-            ticket_categories: Set[TicketCategory]
-    ):
-        self.id = id
-        self.name = name
-        self.venue = venue
-        self.date = date
-        self.tickets = []
-        self.ticket_categories = ticket_categories
-
-    def add_ticket(self, ticket: Ticket):
-        try:
-            if UniqueValidator.is_unique(ticket, self.tickets):
-                if ticket.type in self.venue.existing_ticket_types and ticket.category in self.ticket_categories:
-                    self.tickets.append(ticket)
-            else:
-                raise ValueError("Current event doesn't support tickets of this type.")
-        except ValueError as e:
-            print(e)
+    events: List[Event] = None
 
 
 @dataclass
@@ -63,6 +46,6 @@ class SearchRequest:
     date_to: datetime
     price_from: float
     price_to: float
-    ticket_type: Optional[Set[TicketType]]
-    ticket_category: Optional[Set[TicketCategory]]
+    ticket_type: Set[TicketType]
+    ticket_category: Set[TicketCategory]
     quantity: int
